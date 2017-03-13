1a46efd94e4925b081b4409367c21d0faf374cb6
BrettMerrillX <bmerril2@uoregon.edu>
2017-03-13 14:44:51 -0700

    version 3
    Added the 3 page sliding interface. View 2 holds a camera.

---
+++ CouponHunt/View2.swift
@@ -0,0 +1,70 @@
+//
+//  View2.swift
+//  CouponHunt
+//
+//  Created by Student on 3/5/17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class View2: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var ImageLink: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func OpenCamera(_ sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    @IBAction func openPhotoLibraryButton(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        ImageLink.image = image
+        self.dismiss(animated: true, completion: nil);
+    }
+
+    @IBAction func SaveButton(sender: AnyObject) {
+        let imageData = UIImageJPEGRepresentation(ImageLink.image!, 0.6)
+        let compressedJPGImage = UIImage(data: imageData!)
+        UIImageWriteToSavedPhotosAlbum(compressedJPGImage!, nil, nil, nil)
+        
+        let alert = UIAlertView(title: "Wow",
+                                message: "Your image has been saved to Photo Library!",
+                                delegate: nil,
+                                cancelButtonTitle: "Ok")
+        alert.show()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CouponHunt/ViewController.swift
+++ CouponHunt/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  View2ViewController.swift
 //  CouponHunt
 //
 //  Created by Student on 2/27/17.
@@ -8,18 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,
+UIImagePickerControllerDelegate,
+UINavigationControllerDelegate
+{
 
+    
+    
+    
+    @IBOutlet var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let V1: View1 = View1(nibName: "View1", bundle: nil)
+        
+        
+        let V2: View2 = View2(nibName: "View2", bundle: nil)
+        
+        let V3: View3 = View3(nibName: "View3", bundle: nil)
+        
+        // View1
+        self.addChildViewController(V1)
+        self.scrollView.addSubview(V1.view)
+        V1.didMove(toParentViewController: self)
+        
+        //View2
+        self.addChildViewController(V2)
+        self.scrollView.addSubview(V2.view)
+        V2.didMove(toParentViewController: self)
+        //View3
+        self.addChildViewController(V3)
+        self.scrollView.addSubview(V3.view)
+        V3.didMove(toParentViewController: self)
+        
+        var V2Frame : CGRect = V2.view.frame
+        V2Frame.origin.x = self.view.frame.width
+        V2.view.frame = V2Frame
+        
+        var V3Frame : CGRect = V3.view.frame
+        V3Frame.origin.x = 2 * self.view.frame.width
+        V3.view.frame = V3Frame
+        
+        self.scrollView.contentSize = CGSize(width: self.view.frame.width * 3, height: self.view.frame.size.height)
+        
+        
+        
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    }
 
+    /*
+    // MARK: - Navigation
 
-}
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 
